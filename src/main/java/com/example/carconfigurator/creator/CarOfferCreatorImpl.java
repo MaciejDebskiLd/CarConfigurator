cd3ba5b977e14817a53a5d62edcc1b4efdb77c54
Maciej Debski <maciej.debski@yahoo.com>
2019-10-03 20:02:01 +0200

    Add CarOfferCreator

---
+++ src/main/java/com/example/carconfigurator/creator/CarOfferCreatorImpl.java
@@ -0,0 +1,86 @@
+package com.example.carconfigurator.creator;
+
+import com.example.carconfigurator.car.Car;
+import com.example.carconfigurator.engine.EEngineType;
+import com.example.carconfigurator.engine.Engine;
+import org.springframework.beans.factory.annotation.Value;
+
+public class CarOfferCreatorImpl implements CarOfferCreator {
+
+    private static final String EMPTY_VALUE = "-";
+
+    @Value("${offer.header:#{null}}")
+    private String offerHeader;
+
+    @Value("${offer.footer:#{null}}")
+    private String offerFooter;
+
+    @Override
+    public String createCarOffer(Car car){
+
+        StringBuilder sb = new StringBuilder();
+
+        if (offerHeader != null){
+            sb.append("\n");
+            sb.append(offerHeader);
+        }
+
+        sb.append("\nNazwa: ");
+        sb.append(car.getName());
+
+        sb.append("\nKolor: ");
+        sb.append(car.getColor());
+
+        sb.append("\nKoła: ");
+        sb.append(car.getWheelsSize());
+
+        sb.append("\nSilnik: ");
+        sb.append(createEngineDescription(car.getEngine()));
+
+        if (offerFooter != null){
+            sb.append("\n");
+            sb.append(offerFooter);
+        }
+        return  sb.toString();
+    }
+
+    private String createEngineDescription(Engine engine){
+
+        if (engine==null){
+            return EMPTY_VALUE;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("\n Typ: ");
+        sb.append(mapEngineType(engine.getType()));
+
+        sb.append("\n Nazwa: ");
+        sb.append(engine.getName());
+
+        sb.append("\n Moc (KM): ");
+        sb.append(engine.getHorsepower());
+
+        sb.append("\n Skrzynia biegów: ");
+        sb.append(engine.getTransmission());
+
+        sb.append("\n Pojemność: ");
+        sb.append(engine.getCC());
+
+        return sb.toString();
+    }
+
+    private String mapEngineType(EEngineType type){
+
+        if (type == null){
+            return  EMPTY_VALUE;
+        }
+
+        switch (type){
+            case DIESEL: return "Diesel";
+            case PETROL: return "Petrol";
+        }
+        return EMPTY_VALUE;
+    }
+
+}
